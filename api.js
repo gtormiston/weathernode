15e3c0075ddaf3b4e51bdd7425f81d1a92c277b5
Graham Ormiston <grahamormiston@gmail.com>
2016-09-30 06:12:20 +0100

    added test for consuming api and implemented

---
+++ api.js
@@ -0,0 +1,40 @@
+var http = require('http');
+
+module.exports = {
+	get: function(callback) {
+		var req = http.request({
+			hostname: 'api.openweathermap.org',
+			path: '/data/2.5/weather?q=London,uk&appid=859bb0993d1fd7f249aeb2bd2bbc30d2'
+		}, function(response) {
+			var data = '';
+			response.on('data', function(chunk) {
+				data += chunk;
+			});
+			response.on('end', function() {
+				callback(null, JSON.parse(data));
+			});
+		});
+		req.end();
+	},
+
+	post: function(data, callback) {
+		var req = http.request({
+			hostname: 'jsonplaceholder.typicode.com',
+			path: '/posts',
+			method: 'POST'
+		}, function(response) {
+			var data = '';
+			response.on('data', function(chunk) {
+				data += chunk;
+			});
+
+			response.on('end', function() {
+				callback(null, JSON.parse(data));
+			});
+		});
+
+		req.write(JSON.stringify(data));
+
+		req.end();
+	}
+};
--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var app = express();
 var request = require('request');
+var api = require('./api.js');
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-
-  //   request('http://www.google.com', function (error, response, body) {
-  //     if (!error && response.statusCode == 200) {
-  //       console.log(body) // Show the HTML for the Google homepage.
-  //     }
-  // })
-
+  console.log('WeatherNode listening on port 3000!');
+  api.get(function(err, result) {
+    console.log(result)
+  })//.on(err, function (e) {
+    //console.log(e);
+  //});
 });
